eff60feaf1260134feeb7e6926c3d7ab64ff03f3
yvan vander sanden <yvan@attr-x.net>
2019-01-06 14:49:41 +0200

    Added oef 6

---
+++ oefening/Oefeningen/Oef6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefeningen
+{
+    /*
+     * De class EventManager bepaalt welke gebruiker verantwoordelijk is voor
+     * een event. Een EventManager moet dus een User een een Event hebben. Die
+     * heb je al uitgewerkt in de vorige oefeningen.
+     * 
+     * Bij deze oefening krijg je geen stappenplan meer. Je bepaalt zelf welke
+     * properties, constructor en functies je class nodig heeft. 
+     * 
+     * Je kan de class testen in de functie TestEventManager onder de class.
+     * Daarin zal je ook Users, Events en Locations moeten voorzien om dat te kunnen 
+     * doen.
+     */ 
+    public class EventManager
+    {
+
+    }
+
+    public partial class Tests
+    {
+        public static void TestEventManager()
+        {
+            // test your class here
+        }
+    }
+}
--- oefening/Program.cs
+++ oefening/Program.cs
@@ -13,7 +13,9 @@ namespace First
 			menu.AddOption('1', "Voer Oef1 uit", () => { Oefeningen.Tests.TestCircle(); });
             menu.AddOption('2', "Voer Oef2 uit", () => { Oefeningen.Tests.TestRectangle(); });
             menu.AddOption('3', "Voer Oef3 uit", () => { Oefeningen.Tests.TestUser(); });
-            menu.AddOption('4', "Voer oef4 uit", () => { Oefeningen.Tests.TestLocation(); });
+            menu.AddOption('4', "Voer Oef4 uit", () => { Oefeningen.Tests.TestLocation(); });
+            menu.AddOption('5', "Voer Oef5 uit", () => { Oefeningen.Tests.TestEvent(); });
+            menu.AddOption('6', "Voer Oef6 uit", () => { Oefeningen.Tests.TestEventManager(); });
 
             menu.Start();
 		}
